9478dbc128b58de0f55015c5610a98464724a384
touhidur-rahaman <touhidur.rahaman.antu@gmail.com>
2022-11-27 23:51:04 +0600

    role and permission table

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,10 +1,41 @@
 <x-admin-layout>
-    <div class="py-12">
+    <div class="py-12  w-full">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Admin Roles
+                <div class="flex justify-end p-2">
+                    <a href="" class="px-4 py-2 bg-green-700 hover:bg-green-500 rounded-md">Create</a>
                 </div>
+                <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="py-3 px-6">
+                                    Name
+                                </th>
+                                
+                                <th scope="col" class="py-3 px-6">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($roles as $role)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" class="flex items-center py-4 px-6 text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{$role->name}} 
+                                </th>
+                                
+                                <td class="py-4 px-6">
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                                </td>
+                            </tr>    
+                            @endforeach
+                            
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
     </div>
